88c413c7689c08baede959a32dc680bf1fb32bfd
Johnathan Bangura Jr <jbangura@vt.edu>
2023-09-22 06:02:49 +0000

    #11 install terraform provider, create s3 bucket

--- main.tf
+++ main.tf
@@ -4,16 +4,31 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
 provider "random" {
-  # Configuration options
+
+}
+
+provider "aws" {
+
 }
 
 resource "random_string" "bucket_name" {
-  length  = 16
+  lower   = true
+  upper   = false
+  length  = 32
   special = false
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 
