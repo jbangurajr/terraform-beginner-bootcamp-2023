477e27489424c7ec9789b7444e25f25432dc0104
Johnathan Bangura <135305037+jbangurajr@users.noreply.github.com>
2023-09-24 18:52:16 -0400

    #31 enabled static website hosting ; uploaded index and error files via terraform (#32)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value       = module.terrahouse_aws
+  value = module.terrahouse_aws.bucket_name
+
+}
+
+output "s3_website_endpoint" {
+  description = "S3 Static website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,11 @@ variable "bucket_name" {
   description = "AWS S3 Bucket Name"
   type        = string
 }
+
+variable "index_html_filepath" {
+  type        = string
+}
+
+variable "error_html_filepath" {
+  type        = string
+}
