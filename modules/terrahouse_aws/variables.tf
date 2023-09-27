097abb0100c82c61a4ed2149521def8cf2c45508
Johnathan Bangura <135305037+jbangurajr@users.noreply.github.com>
2023-09-26 23:00:22 -0400

    #36 only upload index.html/error.html if content version has changed (#37)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -37,3 +37,13 @@ variable "error_html_filepath" {
     error_message = "The specified error_html_filepath does not exist or is invalid."
   }
 }
+
+variable "content_version" {
+  description = "The content version. Should be a positive integer starting at 1."
+  type        = number
+
+  validation {
+    condition     = var.content_version > 0 && floor(var.content_version) == var.content_version
+    error_message = "The content_version must be a positive integer starting at 1."
+  }
+}
