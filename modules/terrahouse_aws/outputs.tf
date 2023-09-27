e5e396c6613bcbc44a602f67fe13f4099d79dd4f
Johnathan Bangura Jr <jbangura@vt.edu>
2023-09-27 03:24:44 +0000

    #38 invalidate cloudfront stack when content version changes; output the cloudfront url on apply

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -8,3 +8,7 @@ output "website_endpoint" {
   description = "S3 Static website hosting endpoint"
   value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
 }
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -63,3 +63,15 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     cloudfront_default_certificate = true
   }
 }
+
+resource "terraform_data" "invalidate_cache" {
+  triggers_replace = terraform_data.content_version.output
+
+  provisioner "local-exec" {
+    command = <<EOT
+aws cloudfront create-invalidation \
+--distribution-id ${aws_cloudfront_distribution.s3_distribution.id} \
+--paths '/*'
+                EOT
+  }
+}
