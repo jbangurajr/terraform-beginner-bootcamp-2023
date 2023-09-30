c6e5d2dbadd2d6ca255732e85d2b5a75d5f6e31c
Johnathan Bangura Jr <jbangura@vt.edu>
2023-09-30 18:43:51 +0000

    3rd try

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
-  source = "${path.root}${var.index_html_filepath}"
+  source = "${var.index_html_filepath}"
   content_type = "text/html"
 
   etag = filemd5("${var.index_html_filepath}")
@@ -33,7 +33,7 @@ resource "aws_s3_object" "index_html" {
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
-  source = "${path.root}${var.error_html_filepath}"
+  source = "${var.error_html_filepath}"
   content_type = "text/html"
 
   etag = filemd5("${var.error_html_filepath}")
