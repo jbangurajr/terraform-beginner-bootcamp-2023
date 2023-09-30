434847a3cd0df5a01e8b5c969c28e168579ab81d
Johnathan Bangura Jr <jbangura@vt.edu>
2023-09-30 18:41:52 +0000

    2nd try

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -27,7 +27,7 @@ resource "aws_s3_object" "index_html" {
   source = "${path.root}${var.index_html_filepath}"
   content_type = "text/html"
 
-  etag = filemd5("${path.root}${var.index_html_filepath}")
+  etag = filemd5("${var.index_html_filepath}")
 }
 
 resource "aws_s3_object" "error_html" {
@@ -36,7 +36,7 @@ resource "aws_s3_object" "error_html" {
   source = "${path.root}${var.error_html_filepath}"
   content_type = "text/html"
 
-  etag = filemd5("${path.root}${var.error_html_filepath}")
+  etag = filemd5("${var.error_html_filepath}")
 }
 
 
