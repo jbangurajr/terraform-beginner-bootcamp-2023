578ee8d3c999ab05b9889210ab3d995127a382c1
Johnathan Bangura Jr <jbangura@vt.edu>
2023-10-04 04:17:04 +0000

    #47 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   /*
   cloud {
     organization = "terraform-cloud-bootcamp"
@@ -10,6 +16,13 @@ terraform {
   */
 }
 
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+/*
 module "terrahouse_aws" {
 
   source      = "./modules/terrahouse_aws"
@@ -21,3 +34,4 @@ module "terrahouse_aws" {
   content_version = var.content_version
 
 }
+*/
