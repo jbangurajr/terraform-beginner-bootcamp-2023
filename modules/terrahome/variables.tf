136f703a99dbe81130386992052c3e3b2c48a923
Johnathan Bangura Jr <jbangura@vt.edu>
2023-10-06 23:27:54 +0000

    #57 fix issues with provider, set up terratown homes

--- modules/terrahome/variables.tf
+++ modules/terrahome/variables.tf
@@ -21,24 +21,9 @@ variable "bucket_name" {
 }
 */
 
-variable "index_html_filepath" {
-  description = "Path to the index.html file"
+variable "public_path" {
+  description = "The file path for the public directory"
   type        = string
-
-  validation {
-    condition     = fileexists(var.index_html_filepath)
-    error_message = "The specified index_html_filepath does not exist or is invalid."
-  }
-}
-
-variable "error_html_filepath" {
-  description = "Path to the error.html file"
-  type        = string
-
-  validation {
-    condition     = fileexists(var.error_html_filepath)
-    error_message = "The specified error_html_filepath does not exist or is invalid."
-  }
 }
 
 variable "content_version" {
@@ -49,10 +34,4 @@ variable "content_version" {
     condition     = var.content_version > 0 && floor(var.content_version) == var.content_version
     error_message = "The content_version must be a positive integer starting at 1."
   }
-}
-
-
-variable "assets_path" {
-  description = "Path to assets folder"
-  type = string
 }
